b75e8fbcaa39b4652b836c9796da8e18bfcb482e
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-02-12 23:49:00 +0100

    Added about

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./About.scss";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>Lorem ipsum</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quos
+        impedit cumque optio qui nemo est exercitationem. Totam enim quos omnis
+        cupiditate excepturi? Officiis officia, eveniet rerum earum labore
+        perspiciatis.
+      </p>
+    </div>
+  );
 };
 
 export default About;
