aa1d9dd113f98b841c427ddf61e87fa8d3581067
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-05-05 14:38:20 +0200

    Projects have same size

--- src/data.js
+++ src/data.js
@@ -21,7 +21,7 @@ import {
 } from "react-icons/tb";
 
 import work2 from "./assets/work2.jpg";
-import work3 from "./assets/work3.jpg";
+import work3 from "./assets/work3.1.png";
 import work4 from "./assets/work4.jpg";
 import work5 from "./assets/work5.png";
 
